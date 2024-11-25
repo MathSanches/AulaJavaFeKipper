44fe016658a8e0a559d6fdce2008cb5972c20434
Mmariano <m.marianosanches@gmail.com>
2024-11-24 22:11:08 -0300

    começo do código em java, aula de 1 hora da kipper

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // numero inteiro
+        //byte => 8 bits => -128 ---até--- 127
+        //short => 16 bits => -32.768 ---até--- 32.767
+        //int => 32 bits => -2.147.483.648 ---até--- 2.147.483.647
+        //long => 64 bits => -9.223.372.036.854.775.808 ---até--- 9.223.372.036.854.775.807
+
+        //numeros decimais
+        //float => 32 bits => precisão simples
+        //double => 64 bits => precisão dupla
+    }
+}
